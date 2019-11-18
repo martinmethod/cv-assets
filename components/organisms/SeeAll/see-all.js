0adce0815359a02d8297869759639e72dadc3b76
Martin Metodiev <metodiev.martin@gmail.com>
2019-11-18 07:51:01 +0200

    Add X-close button to Modal

--- components/organisms/SeeAll/see-all.js
+++ components/organisms/SeeAll/see-all.js
@@ -5,9 +5,10 @@
 import sections from '@/node_modules/cv-assets/components/sections';
 import Modal from '@/node_modules/cv-assets/components/overlays/Modal';
 import DotButton from '@/node_modules/cv-assets/components/atoms/DotButton';
+import X from '@/node_modules/cv-assets/assets/graphics/x.svg';
 
 export default {
-  components: { Modal, DotButton },
+  components: { Modal, DotButton, X },
 
   props: {
     content: {
---
+++ components/overlays/Modal/index.vue
@@ -0,0 +1,3 @@
+<template src="./modal.html"></template>
+<script src="./modal.js"></script>
+<style src="./modal.scss" module lang="scss"></style>
