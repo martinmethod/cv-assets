ded3b8bac995f42879b5342e3e72887b8945addf
Martin Metodiev <metodiev.martin@gmail.com>
2021-01-14 20:51:23 +0200

    Fix BG locale for 'moment-duration-format'

--- plugins/moment.js
+++ plugins/moment.js
@@ -4,5 +4,26 @@ import momentDurationFormatSetup from 'moment-duration-format';
 momentDurationFormatSetup(moment);
 
 export default ({ app }) => {
+  moment.updateLocale('bg', {
+    durationLabelsStandard: {
+      S: "милисекунда",
+      SS: "милисекунди",
+      s: "секунда",
+      ss: "секунди",
+      m: "минута",
+      mm: "минути",
+      h: "час",
+      hh: "часа",
+      d: "ден",
+      dd: "дни",
+      w: "седмица",
+      ww: "седмици",
+      M: "месец",
+      MM: "месеца",
+      y: "година",
+      yy: "години"
+    }
+  });
+
   moment.locale(app.i18n.locale);
 };
