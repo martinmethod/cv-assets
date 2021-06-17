40deb71b47b22af072b83ad7d303974515fb706c
Martin Metodiev <metodiev.martin@gmail.com>
2021-06-17 17:19:23 +0300

    Add hyperlink support to Progress bars

--- components/atoms/ProgressBar/progress-bar.js
+++ components/atoms/ProgressBar/progress-bar.js
@@ -10,6 +10,26 @@ export default {
     label: {
       type: [String, null],
       default: null
+    },
+    hyperlink: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    barComponent() {
+      return this.hyperlink ? {
+        element: 'a',
+        attrs: {
+          ['data-btn']: true,
+          href: this.hyperlink,
+          target: '_blank'
+        }
+      } : {
+        element: 'div',
+        attrs: {}
+      }
     }
   }
 };
