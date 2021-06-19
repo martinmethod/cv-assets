f29ff0d1d6fb3db1f754aa7ad57e4c1c8d232323
Martin Metodiev <metodiev.martin@gmail.com>
2021-06-19 15:21:48 +0300

    Improve “dir” configuration to support argument

--- config/nuxt/dir.js
+++ config/nuxt/dir.js
@@ -1,7 +1,7 @@
-export default {
+export default (dirs = {}) => Object.assign({
   assets: 'node_modules/cv-assets/assets',
   layouts: 'node_modules/cv-assets/layouts',
   pages: 'node_modules/cv-assets/pages',
   static: 'node_modules/cv-assets/static',
   store: 'node_modules/cv-assets/store'
-}
+}, dirs);
